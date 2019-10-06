e2593adfd134f0b9228c7a56e4444ad0b682383d
Naufal Asha <55681676+NaufalAshaW@users.noreply.github.com>
2019-10-06 19:12:35 +0700

    Add files via upload

---
+++ src/Kelipatan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * NAMA   : Naufal Asha
+ * KELAS  : IF-2
+ * NIM    : 10118063
+ */
+public class Kelipatan {
+
+    /**
+     * @param args
+     */
+      public static void main(String[] args){
+          for(Double i = 3.5; i <= 35; i+= 3.5){
+              System.out.println(i);
+          }
+    }
+}
